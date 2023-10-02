6c3bbff44d8d809b6cada36a3650ce676f864622
alykrauze <alicekrauze@alices-MacBook-Pro.local>
2023-10-02 13:56:37 -0400

    update java

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,6 @@
+// URL for json data
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+// fetch json data & initlizing drop down
 
 function graphs() {
     let dropdownbar = d3.select('#selDataset');
@@ -19,6 +21,8 @@ function graphs() {
     });
 }
 
+//creating trace bar chart, data array, and plotting bar chart 
+
 function bar(x) {
     d3.json(url).then((data) => {
         console.log(data);
@@ -29,6 +33,7 @@ function bar(x) {
 
         let valueData = value[0];
 
+        //creating trace 
         let trace = [{
             x: valueData.sample_values.slice(0,10).reverse(),
             y: valueData.otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse(),
@@ -37,20 +42,24 @@ function bar(x) {
             orientation: 'h'
         }];
 
+        //creating plot 
         Plotly.newPlot('bar', trace);
     })
 }
 
+// create trace for bubble chart, data array, and plotting
 
 function bubble(x) {
     d3.json(url).then((data) => {
 
+        // setting values 
         let results = data.samples;
 
         let value = results.filter(result => result.id == x);
 
         let valueData = value[0];
 
+        //trace for bubble
         let trace = [{
             x: valueData.otu_ids,
             y: valueData.sample_values,
@@ -63,13 +72,18 @@ function bubble(x) {
             }
         }];
 
+        //layout for bubble 
         let layout = {
             xaxis: {title: 'OTU ID'}
         };
 
+        //plotting bubble 
         Plotly.newPlot('bubble', trace, layout);
     });
 }
+
+
+//individuals demographic data info 
 
 function demographic(x) {
     d3.json(url).then((data) => {
@@ -94,6 +108,8 @@ function demographic(x) {
     });
 }
 
+
+//handle drop down change event 
 function change_option(x) {
 
     bar(x);
@@ -102,6 +118,7 @@ function change_option(x) {
 
 }
 
+//initilize charts 
 graphs()
 
 
